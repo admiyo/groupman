6d023d5bb2ffa8dcbbb75f3dfe64ae1e1d0f9070
Adam Young <ayoung@redhat.com>
2019-06-13 11:19:39 -0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Distutils',
+      version='1.0',
+      description='Group M<anager for Linux',
+      author='Adam Young',
+      author_email='adam@younglogic.com',
+      url='https://www.younglogic.com/',
+      packages=['groupman'],
+     )
